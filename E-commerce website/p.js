a678a7b5fb819113dc9dfc753f5cac0abafb0f14
nishantkr33 <nishant2432004@gmail.com>
2024-10-30 14:15:15 +0530

    Create p.js

---
+++ E-commerce website/p.js
@@ -0,0 +1,11 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  image: String,
+});
+
+module.exports = mongoose.model('Product', productSchema);
